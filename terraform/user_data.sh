bb0e00f79d7ddf9f8572be7497fa996fed22432a
Bryce Boe <bbzbryce@gmail.com>
2021-01-07 14:39:07 -0800

    Ensure essential python and ruby packages are installed

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 yum update -y
 yum install -y emacs-nox gcc git httpd-tools libffi-devel openssl-devel python3 python3-devel
-amazon-linux-extras install ruby2.6
 pip3 install --upgrade pip
-/usr/local/bin/pip install awsebcli --upgrade
+/usr/local/bin/pip install PyJWT==1.6.1 awsebcli --upgrade
+amazon-linux-extras install ruby2.6
+gem install jwt
