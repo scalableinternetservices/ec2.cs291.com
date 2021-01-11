bb0e0091c1d58e5130dd7a5f5c956c35bb04d864
Bryce Boe <bbzbryce@gmail.com>
2021-01-10 21:18:23 -0800

    Ensure jq is installed

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum update -y
-yum install -y emacs-nox gcc git httpd-tools libffi-devel openssl-devel python3 python3-devel
+yum install -y emacs-nox gcc git httpd-tools jq libffi-devel openssl-devel python3 python3-devel
 pip3 install --upgrade pip
 /usr/local/bin/pip install PyJWT==1.6.1 awsebcli --upgrade
 amazon-linux-extras install ruby2.6
