ab4b5257f040a301df473535ffab4db73baad829
Bryce Boe <bbzbryce@gmail.com>
2021-10-27 16:15:38 -0700

    Update launch_tsung script for new AMI and default vpc security group

--- launch_tsung.sh
+++ launch_tsung.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 TEAMNAME=$(whoami)
-TSUNG_AMI=ami-009897500d3692850
+TSUNG_AMI=ami-0085826a91a6d1ec3
 
 if [ $# -eq 0 ]; then
     instance_type=t3.micro
@@ -18,15 +18,14 @@ result=$(aws ec2 run-instances \
     --instance-type=$instance_type \
     --key-name=$TEAMNAME \
     --monitoring=Enabled=True \
-    --security-groups='["allow_http", "allow_ssh", "outbound_http", "outbound_tls"]' \
+    --security-groups='["tsung"]' \
     --tag-specifications='[{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"tsung-'$TEAMNAME'"}]},{"ResourceType":"volume","Tags":[{"Key":"Name","Value":"tsung-'$TEAMNAME'"}]}]')
 
 id=$(echo $result | jq -r '.Instances[].InstanceId')
 
 echo "Instance $id is launching. Obtaining IP address..."
-
+sleep 1
 public_ip=$(aws ec2 describe-instances --filters Name=instance-id,Values=$id | jq -r .Reservations[].Instances[].PublicIpAddress)
-
 
 echo -e "SSH\n---\nssh ec2-user@$public_ip\n"
 echo -e "HTTP\n----\nhttp://$public_ip"
